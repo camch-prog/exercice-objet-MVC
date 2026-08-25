2b7a73c5567fb0cf199c349a5d148907a835b9f2
camch <charassoncamille@gmail.com>
2026-08-25 16:43:39 +0200

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e269c018c7b51b5717d803737b6a5a043f1bb1bc',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e269c018c7b51b5717d803737b6a5a043f1bb1bc',
+            'dev_requirement' => false,
+        ),
+    ),
+);
