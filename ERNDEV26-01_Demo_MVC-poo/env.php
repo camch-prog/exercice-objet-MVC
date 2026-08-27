ba9708ab3b0f1332df6c444f9a6e5ae88f320838
camch <charassoncamille@gmail.com>
2026-08-27 16:56:02 +0200

    Add files via upload

--- ERNDEV26-01_Demo_MVC-poo/env.php
+++ ERNDEV26-01_Demo_MVC-poo/env.php
@@ -1,4 +1,6 @@
 <?php
 //Listes des Routes
-$_ENV['utilisateurs'] = "/MVC/";
-$_ENV['articles'] = "/MVC/articles";
+$_ENV['utilisateurs'] = "/";
+$_ENV['articles'] = "/articles";
+$_ENV['compte'] = "/compte";
+$_ENV['deco'] ="/deco";
--- ERNDEV26-01_Demo_MVC-poo/model/modelArticle.php
+++ ERNDEV26-01_Demo_MVC-poo/model/modelArticle.php
@@ -1,18 +1,37 @@
 <?php
-function getArticles(){
-    try{
-        $bdd = connect();
-        //1. Preparer la requête
-        $request = 'SELECT a.title, a.content, a.created_at, a.edited_at, u.pseudo FROM article a INNER JOIN user u ON u.id = a.user_id';
+namespace Model;
 
-        $req = $bdd->prepare($request);
+use PDO, EXCEPTION;
+use Model\Model;
 
-        //2. Exécution de la requête
-        $req->execute();
+class ModelArticle extends Model{
+    //ATTRIBUT
+    private ?int $id;
+    private ?string $title;
+    private ?string $content;
+    private ?string $createdAt;
+    private ?string $editedAt;
+    private ?string $author;
 
-        //3. Retourner les données
-        return $req->fetchAll(PDO::FETCH_ASSOC);
-    }catch(EXCEPTION $error){
-        die($error->getMessage());
+    //CONSTRUCTEUR
+    
+    //GETTER ET SETTER
+
+    //METHODS
+    public function findAll():?array{
+        try{
+            //1. Preparer la requête
+            $request = 'SELECT a.id, a.title, a.content, a.created_at, a.edited_at, u.pseudo FROM article a INNER JOIN user u ON u.id = a.user_id';
+
+            $req = $this->getBDD()->prepare($request);
+
+            //2. Exécution de la requête
+            $req->execute();
+
+            //3. Retourner les données
+            return $req->fetchAll(PDO::FETCH_ASSOC);
+        }catch(EXCEPTION $error){
+            die($error->getMessage());
+        }
     }
 }
